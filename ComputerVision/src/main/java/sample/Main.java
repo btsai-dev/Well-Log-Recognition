469b96a2deedf179aacb9d1ec5e901390bc762b4
godonan <godonanabt@gmail.cm>
2020-05-17 15:19:56 -0500

    Full Menu Redesign Part 01
    Menu fully redesigned.
    Added keyword adding.
    Added keyword addition to scrollpane.
    Added clearing keywords.

--- ComputerVision/src/main/java/sample/Main.java
+++ ComputerVision/src/main/java/sample/Main.java
@@ -12,35 +12,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        //LaunchConfiguration();
         LaunchMainMenu();
-    }
-
-    public void LaunchConfiguration() throws IOException {
-        String path = System.getProperty("user.home") +
-                File.separator+ "WellLogAnalysis" +
-                File.separator+"config.properties";
-        File config = new File(path);
-        config.getParentFile().mkdirs();
-        Controller.configPath = path;
-
-        Stage configWindow = new Stage();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Configurator.fxml"));
-        configWindow.setTitle("Configuration");
-        configWindow.setScene(new Scene(root, 600, 400));
-        configWindow.setResizable(false);
-        configWindow.showAndWait();
-
-        Controller.setImageFilter(ImageUtils.jpeg, ImageUtils.jpg, ImageUtils.png, ImageUtils.gif, ImageUtils.tif, ImageUtils.tiff);
     }
 
     public void LaunchMainMenu() throws IOException {
         Stage menuStage = new Stage();
-        // FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MainMenu.fxml"));
         menuStage.setTitle("Main Menu");
         menuStage.setScene(new Scene(root, 800, 600));
-        //menuStage.setResizable(false);
         menuStage.setMaximized(true);
         menuStage.setMinWidth(800);
         menuStage.setMinHeight(600);
@@ -48,10 +27,32 @@ public class Main extends Application {
         menuStage.showAndWait();
     }
 
-    public static void main(String[] args){
+    /**
+     * Launches the configuration window
+     * @throws IOException
+     */
+    public void LaunchConfiguration() throws IOException {
+        Stage configWindow = new Stage();
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Configurator.fxml"));
+            configWindow.setTitle("Configuration");
+            configWindow.setScene(new Scene(root, 600, 400));
+            configWindow.setResizable(false);
+            configWindow.showAndWait();
+        } catch(IOException e){
+            System.out.println(e.getStackTrace());
+            System.exit(0);
+        } catch(Exception e){
+            System.out.println(e.getStackTrace());
+            System.exit(0);
+        }
 
-        final boolean LAUNCH = true;
-        if (LAUNCH)
-            launch(args);
+        Controller.setImageFilter(ImageUtils.jpeg, ImageUtils.jpg, ImageUtils.png, ImageUtils.gif, ImageUtils.tif, ImageUtils.tiff);
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
+
+
